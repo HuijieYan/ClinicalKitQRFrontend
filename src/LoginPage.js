6e4d7b603a6d668b8aba293704fcbee7d45aa3b3
unknown <zcabpl0@ucl.ac.uk>
2021-12-17 13:17:04 +0000

    connected login page with backend

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,17 +1,33 @@
 import { useHistory } from "react-router";
 import './LoginPage.css';
 import { Button, Form } from "react-bootstrap";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const LoginPage = () => {
     const history = useHistory();
     const loginFail = false;
+    const hospitalID = 1;
+    const loginpageURL = "http://localhost:8080/login";
+    const [username,setUsername] = useState("");
+    const [password,setPassword] = useState("");
+    const [result,setResult] = useState(false);
+
     const login = (history) =>{
-        if(loginFail){
+        if(result === false){
             history.push("/loginFail");
         }else{
             history.push("/home");
         } 
     }
+
+    useEffect(()=>{
+        var url = loginpageURL + "/hospitalID=" + hospitalID + " username=" + username + " password=" + password;
+        axios.get(url).then((response)=>{
+            const result = response.data;
+            setResult(result);
+        });
+    })
 
     return ( 
         <div className = "inputField">
@@ -19,13 +35,13 @@ const LoginPage = () => {
             <Form>
                 <Form.Group id="username">
                     <Form.Label>Departmental Username</Form.Label>
-                    <Form.Control type="username" placeholder="Enter Departmental Username"></Form.Control>
+                    <Form.Control type="username" placeholder="Enter Departmental Username" value={username} onChange={(e)=>setUsername(e.target.value)}></Form.Control>
                 </Form.Group>
             </Form>
             <Form>
                 <Form.Group id="password">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Department Password"></Form.Control>
+                    <Form.Control type="password" placeholder="Department Password" value={password} onChange={(e)=>setPassword(e.target.value)}></Form.Control>
                 </Form.Group>
             </Form>
             <Button id="loginButton" type="submit" onClick={()=>login(history)}>Log in</Button>
