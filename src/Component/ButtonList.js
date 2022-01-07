1f41341c629a2c7d6c2114c7fa8480a1c7ea5021
safiyya-ona <safiyya.onanuga@hotmail.co.uk>
2022-01-07 18:04:46 +0000

    Added Icons on IndexMain.js page (Home Page)

--- src/Component/ButtonList.js
+++ src/Component/ButtonList.js
@@ -1,10 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import { useHistory } from "react-router-dom";
 import { Button } from "react-bootstrap";
+import { AiFillMedicineBox } from "react-icons/ai";
+import { RiGroupLine } from "react-icons/ri";
+import { GoReport } from "react-icons/go";
+import { HiOutlineDocumentReport } from "react-icons/hi";
 
 const ButtonList = () => {
     const history = useHistory();
-
     return (
         <section className="p-5">
             <div className="container">
@@ -13,7 +16,7 @@ const ButtonList = () => {
                         <div className="card bg-dark text-light">
                             <div className="card-body text-center">
                                 <div className="h1 mb-3">
-                                    <label>Wait icon</label>
+                                    <AiFillMedicineBox />
                                 </div>
                                 <h3 className="card-title mb-3">Equipment</h3>
                                 <p className="card-text">
@@ -28,7 +31,7 @@ const ButtonList = () => {
                         <div className="card bg-dark text-light">
                             <div className="card-body text-center">
                                 <div className="h1 mb-3">
-                                    <label>Wait icon</label>
+                                    <RiGroupLine />
                                 </div>
                                 <h3 className="card-title mb-3">User Group</h3>
                                 <p className="card-text">
@@ -43,7 +46,7 @@ const ButtonList = () => {
                         <div className="card bg-dark text-light">
                             <div className="card-body text-center">
                                 <div className="h1 mb-3">
-                                    <label>Wait icon</label>
+                                    < GoReport />
                                 </div>
                                 <h3 className="card-title mb-3">Reported Issues</h3>
                                 <p className="card-text">
@@ -58,7 +61,7 @@ const ButtonList = () => {
                         <div className="card bg-dark text-light">
                             <div className="card-body text-center">
                                 <div className="h1 mb-3">
-                                    <label>Wait icon</label>
+                                    <HiOutlineDocumentReport />
                                 </div>
                                 <h3 className="card-title mb-3">Produce Report</h3>
                                 <p className="card-text">
