9df46491157a63bbeb350044bfbf5b98e64d3037
unknown <zcabpl0@ucl.ac.uk>
2022-01-12 15:04:53 +0000

    Refactored Equipment Table

--- src/Pages/muiTable.js
+++ src/Pages/muiTable.js
@@ -2,10 +2,6 @@ import MUIDataTable, { TableToolbar } from "mui-datatables";
 import { useEffect, useState } from "react";
 import { ThemeProvider } from "@mui/styles";
 import { createTheme } from "@mui/material/styles";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
 import axios from "axios";
 import UserStatus from "../Component/UserStatus";
 import GetData from "../Functions/GetData";
@@ -25,15 +21,27 @@ const EquipmentTable = () => {
                 var rowsData = [];
                 for (let i = 0;i<data.length;i++){
                     var equipment = data[i];
-                    rowsData.push([equipment.name,equipment.equipmentId,equipment.hospitalId.hospitalName,<a href={URL+equipment.equipmentId}>QR code</a>]);
+                    rowsData.push({name:equipment.name,id:equipment.equipmentId,hospital:equipment.hospitalId.hospitalName,qr:<a href={URL+equipment.equipmentId}>QR code</a>});
                 }
                 setRows(rowsData);
             })
         }
         if (level === 2){
             setColumns([
-                "Equipment Name",
-                "Equipment ID",
+                { name: "name",
+                    label: "Equipment Name",
+                    options: {
+                        filterOptions: { fullWidth: true },
+                        viewColumns: false
+                    }
+                },
+                { name: "id",
+                    label: "Equipment ID",
+                    options: {
+                        filterOptions: { fullWidth: true },
+                        viewColumns: false
+                    }
+                },
                 { name: "hospital",
                     options: {
                         filterOptions: { fullWidth: true },
@@ -41,7 +49,13 @@ const EquipmentTable = () => {
                         viewColumns: false
                     }
                 },
-                "QR Code",
+                { name: "qr",
+                    label: "QR Code",
+                    options: {
+                        filterOptions: { fullWidth: true },
+                        viewColumns: false
+                    }
+                },
             ])
             GetData.getAllEquipmentByHospital(UserStatus.getHospitalId()).then((data)=>{
                 var rowsData = [];
@@ -57,10 +71,33 @@ const EquipmentTable = () => {
     //renders only once for fetching selection options
 
     const [columns,setColumns] = useState([
-        "Equipment Name",
-        "Equipment ID",
-        "hospital",
-        "QR Code",
+        { name: "name",
+                    label: "Equipment Name",
+                    options: {
+                        filterOptions: { fullWidth: true },
+                        viewColumns: false
+                    }
+                },
+                { name: "id",
+                    label: "Equipment ID",
+                    options: {
+                        filterOptions: { fullWidth: true },
+                        viewColumns: false
+                    }
+                },
+                { name: "hospital",
+                    options: {
+                        filterOptions: { fullWidth: true },
+                        viewColumns: false
+                    }
+                },
+                { name: "qr",
+                    label: "QR Code",
+                    options: {
+                        filterOptions: { fullWidth: true },
+                        viewColumns: false
+                    }
+                },
     ]);
 
     const options = {
@@ -68,22 +105,21 @@ const EquipmentTable = () => {
         tableBodyHeight,
         jumpToPage: true,
         onRowSelectionChange:function(currentRowsSelected, allRowsSelected, rowsSelected){
-            setSelected(allRowsSelected);
-        }
+            setSelected(rowsSelected);
+        },
+        onRowsDelete:function(){
+            var rowLs = rows;
+            for (let i = 0;i<selected.length;i++){
+                var index = rowLs[selected[i]];
+                console.log("http://localhost:8080/equipment/delete/id="+rowLs[i].id)
+                axios.delete("http://localhost:8080/equipment/delete/id="+rowLs[i].id);
+                rowLs.splice(index,1);
+            }
+            setRows(rowLs);
+            setSelected([]);
+            return true;
+        },
     };
-    
-    
-
-    const handleDelete = (event,rows,setRows, selected,setSelected) =>{
-        var rowLs = rows;
-        for (let i = 0;i<selected.length;i++){
-            var index = rowLs.indexOf(selected[i]);
-            axios.delete("http://localhost:8080/equipment/delete/id="+rowLs[i].equipmentID);
-            rowLs.splice(index,1);
-        }
-        setRows(rowLs);
-        setSelected([]);
-    }
 
 
     return (
