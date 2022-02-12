d835b6126899f0bfcde8a0d08899844c9494fc41
safiyya-ona <safiyya.onanuga@hotmail.co.uk>
2022-02-12 20:21:08 +0000

    Slight improvements to admin home page

--- src/Component/ButtonList.js
+++ src/Component/ButtonList.js
@@ -1,112 +1,106 @@
-import 'bootstrap/dist/css/bootstrap.css';
 import { useHistory } from "react-router-dom";
-import { Button } from "react-bootstrap";
 import { AiFillMedicineBox } from "react-icons/ai";
 import { RiGroupLine } from "react-icons/ri";
 import { GoReport } from "react-icons/go";
 import { HiOutlineDocumentReport } from "react-icons/hi";
+import "./HomePageActionCards.css";
 
 const ButtonList = () => {
-    const history = useHistory();
-    return (
-        <section className="p-5">
-            <div className="container">
-                <div className="row text-center g-4">
-                    <div className="col-md">
-                        <div className="card bg-dark text-light">
-                            <div className="card-body text-center">
-                                <div className="h1 mb-3">
-                                    <AiFillMedicineBox />
-                                </div>
-                                <h3 className="card-title mb-3">Equipment</h3>
-                                <p className="card-text">
-                                    This is description!This is description!This is description!
-                                    This is description!This is description!This is description!
-                                </p>
-                                <Button onClick={()=>history.push("/equipmentTable")}>Check</Button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md">
-                        <div className="card bg-dark text-light">
-                            <div className="card-body text-center">
-                                <div className="h1 mb-3">
-                                    <RiGroupLine />
-                                </div>
-                                <h3 className="card-title mb-3">User Group</h3>
-                                <p className="card-text">
-                                    This is description!This is description!This is description!
-                                    This is description!This is description!This is description!
-                                </p>
-                                <Button onClick={()=>history.push("/usergroupTable")}>Check</Button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md">
-                        <div className="card bg-dark text-light">
-                            <div className="card-body text-center">
-                                <div className="h1 mb-3">
-                                    < GoReport />
-                                </div>
-                                <h3 className="card-title mb-3">Reported Issues</h3>
-                                <p className="card-text">
-                                    This is description!This is description!This is description!
-                                    This is description!This is description!This is description!
-                                </p>
-                                <Button onClick={()=>history.push("/issueTable")}>Check</Button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md">
-                        <div className="card bg-dark text-light">
-                            <div className="card-body text-center">
-                                <div className="h1 mb-3">
-                                    <HiOutlineDocumentReport />
-                                </div>
-                                <h3 className="card-title mb-3">Conatct Book</h3>
-                                <p className="card-text">
-                                    This is description!This is description!This is description!
-                                    This is description!This is description!This is description!
-                                </p>
-                                <Button onClick={()=>history.push("/contactbook")}>Check</Button>
-                            </div>
-                        </div>
-                    </div>
+  const history = useHistory();
+  return (
+    <section className="p-5">
+      <div className="container">
+        <div className="row text-center g-4">
+          <div className="col-md">
+            <div
+              className="card bg-dark d-flex text-light h-100 "
+              onClick={() => history.push("/equipmentTable")}
+            >
+              <div className="card-body justify-content-center d-flex align-items-center">
+                <div>
+                  <div className="h1 mb-3">
+                    <AiFillMedicineBox />
+                  </div>
+                  <h3 className="card-title mb-3">Equipment</h3>
                 </div>
-                <div className="col-md">
-                        <div className="card bg-dark text-light">
-                            <div className="card-body text-center">
-                                <div className="h1 mb-3">
-                                    <HiOutlineDocumentReport />
-                                </div>
-                                <h3 className="card-title mb-3">Inbox</h3>
-                                <p className="card-text">
-                                    This is description!This is description!This is description!
-                                    This is description!This is description!This is description!
-                                </p>
-                                <Button onClick={()=>history.push("/inbox")}>Check</Button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-md">
-                        <div className="card bg-dark text-light">
-                            <div className="card-body text-center">
-                                <div className="h1 mb-3">
-                                    <HiOutlineDocumentReport />
-                                </div>
-                                <h3 className="card-title mb-3">Produce Report</h3>
-                                <p className="card-text">
-                                    This is description!This is description!This is description!
-                                    This is description!This is description!This is description!
-                                </p>
-                                <a href="/" className="btn btn-primary">Check</a>
-                            </div>
-                        </div>
-                    </div>
+              </div>
             </div>
-        </section>
-     );
-}
- 
+          </div>
+          <div className="col-md">
+            <div
+              className="card bg-dark text-light h-100"
+              onClick={() => history.push("/usergroupTable")}
+            >
+              <div className="card-body justify-content-center d-flex align-items-center">
+                <div>
+                  <div className="h1 mb-3">
+                    <RiGroupLine />
+                  </div>
+                  <h3 className="card-title mb-3">User Group</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md">
+            <div
+              className="card bg-dark text-light h-100"
+              onClick={() => history.push("/issueTable")}
+            >
+              <div className="card-body justify-content-center d-flex align-items-center">
+                <div>
+                  <div className="h1 mb-3">
+                    <GoReport />
+                  </div>
+                  <h3 className="card-title mb-3">Reported Issues</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md">
+            <div
+              className="card bg-dark text-light h-100"
+              onClick={() => history.push("/contactbook")}
+            >
+              <div className="card-body justify-content-center d-flex align-items-center">
+                <div>
+                  <div className="h1 mb-3">
+                    <HiOutlineDocumentReport />
+                  </div>
+                  <h3 className="card-title mb-3">Contact Book</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md">
+            <div
+              className="card bg-dark text-light h-100"
+              onClick={() => history.push("/inbox")}
+            >
+              <div className="card-body justify-content-center d-flex align-items-center">
+                <div>
+                  <div className="h1 mb-3">
+                    <HiOutlineDocumentReport />
+                  </div>
+                  <h3 className="card-title mb-3">Inbox</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md">
+            <div className="card bg-dark text-light h-100">
+              <div className="card-body text-center">
+                <div className="h1 mb-3">
+                  <HiOutlineDocumentReport />
+                </div>
+                <h3 className="card-title mb-3">Produce Report</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default ButtonList;
--- src/Pages/IndexMain.js
+++ src/Pages/IndexMain.js
@@ -1,57 +1,80 @@
 import ButtonList from "../Component/ButtonList";
-import {FaQuestion} from "react-icons/fa";
-import 'bootstrap/dist/css/bootstrap.css';
-import { getExpireTime, getHospitalId, getLevel, getUserName } from "../Component/UserStatus";
-import { Button } from "react-bootstrap";
+import { FaQuestion } from "react-icons/fa";
+import "bootstrap/dist/css/bootstrap.css";
+import {
+  getExpireTime,
+  getHospitalId,
+  getLevel,
+  getUserName,
+} from "../Component/UserStatus";
+import { Button, Row } from "react-bootstrap";
 import { logout } from "../Functions/LoginFunctions";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-
+// import HomePageActionCards from "../Component/HomePageActionCards";
 
 const IndexMain = () => {
-    const history = useHistory();
-    const handleLogout = ()=>{
-        logout();
-        history.push("/login");
-    }
+  const history = useHistory();
+  const handleLogout = () => {
+    logout();
+    history.push("/login");
+  };
 
-    return (
-        <div className = "indexMain" id = "mainContent">
-            {console.log(getUserName())}
-            {console.log(getHospitalId())}
-            {console.log(getLevel())}
-            {console.log(getExpireTime())}
-            
-            <section className="bg-dark text-light p-5 p-lg-0 pt-lg-5 text-center text-sm-start">
-                <div className="container">
-                    <div className="d-sm-flex align-items-center justify-content-between">
-                        <div>
-                            <h1><span className="text-warning"> Welcome </span></h1>
-                            <p className="lead my-4">
-                                This is description!This is description!This is description!<br></br>
-                                This is description!This is description!This is description!<br></br>
-                                This is description!This is description!This is description!<br></br>
-                                This is description!This is description!This is description!<br></br>
-                                The background is waiting a picture!!!!<br></br>
-                            </p>
-                        </div>
-                        <img
-                            className="img-fluid w-50 d-none d-sm-block"
-                            src="./defaultProfile.png"
-                            alt=""
-                        />
-                    </div>
-                </div>
-            </section>
-            <div className="float-end" id = "faqButton">
-                <a href="/"><FaQuestion size="20px"/></a>
+  return (
+    <div className="indexMain" id="mainContent">
+      {console.log(getUserName())}
+      {console.log(getHospitalId())}
+      {console.log(getLevel())}
+      {console.log(getExpireTime())}
+
+      <section className="bg-dark text-light p-5 p-lg-0 pt-lg-5 text-center text-sm-start">
+        <div className="container">
+          <div className="d-sm-flex align-items-center justify-content-between">
+            <div>
+              <h1>
+                <span className="text-warning"> Welcome </span>
+              </h1>
+              <p className="lead my-4">
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+              </p>
             </div>
-            <ButtonList/>
-
-            <Button onClick={(e)=>{handleLogout()}}>Log Out</Button>
-
-            <img className="images" src={"http://localhost:8080/file/download/bb5fa948-db14-4543-a8aa-fc4198d81747.png"} alt={""}/>
+            <img
+              className="img-fluid w-50 d-none d-sm-block"
+              src="./defaultProfile.png"
+              alt=""
+            />
+          </div>
         </div>
-     );
-}
- 
+      </section>
+      <div className="float-end" id="faqButton">
+        <a href="/">
+          <FaQuestion size="20px" />
+        </a>
+      </div>
+      <Row className="mb-3 justify-content-center">
+        {/* <HomePageActionCards /> */}
+        <ButtonList />
+      </Row>
+
+      <Button
+        onClick={(e) => {
+          handleLogout();
+        }}
+      >
+        Log Out
+      </Button>
+
+      <img
+        className="images"
+        src={
+          "http://localhost:8080/file/download/bb5fa948-db14-4543-a8aa-fc4198d81747.png"
+        }
+        alt={""}
+      />
+    </div>
+  );
+};
+
 export default IndexMain;
